f2ec0ad98858531a0190138c886ebc748c2e0bc7
Sarah Grant <sarah.grant.ietf@gmail.com>
2024-11-23 09:33:55 +0000

    Fixing Merkle Tree hash test

--- test/merkle_test.exs
+++ test/merkle_test.exs
@@ -2,7 +2,7 @@ defmodule Roughtime.MerkleCryptoTest do
   use ExUnit.Case
 
   test "Hashes correctly" do
-    expected = :crypto.hash(:sha512, "roughtime")
+    expected = <<:crypto.hash(:sha512, "roughtime")::binary-size(32)>>
     got = Roughtime.MerkleCrypto.hash("roughtime")
     assert got == expected
   end
